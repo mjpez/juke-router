177007eb3ca4ce4f3fbbf79d5246f885f98f2988
William Lee <williamlee021@gmail.com>
2017-11-28 16:23:06 -0500

    created a component

---
+++ browser/react/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render(){
+    return (
+      <div id="main" className="container-fluid">
+        <h1>Hello world!</h1>
+      </div>
+    )
+  }
+}
+
+export default Main
